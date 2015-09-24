d0b500cca461e51eeb944ee4d5182ae66a295bb3
shimazug <shimazug@marv.jp>
2015-09-24 16:17:16 +0900

    add スコア集計

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scorer : MonoBehaviour {
+
+	// 累計獲得枚数.
+	private int ScoreCount;
+
+	void OnTriggerEnter(Collider col)
+	{
+		ScoreCount += CountUp (col.GetComponent<Coin>().GetMyColor);
+		Destroy (col.gameObject);
+	}
+
+	int CountUp(bool coin)
+	{
+		int add = 0;
+		if (true == coin) 
+		{
+			add = 10;
+		} 
+		else 
+		{
+			add = 1;
+		}
+		return add;
+	}
+
+}
