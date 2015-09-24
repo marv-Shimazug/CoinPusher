04b3ca16b9aa393f2510b414544afb4c3f96ee25
shimazug <shimazug@marv.jp>
2015-09-24 14:57:57 +0900

    add プッシャー処理用意

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pusher : MonoBehaviour {
+
+	private Vector3 origin;
+
+	void Awake()
+	{
+		// 初期位置の保存.
+		origin = this.transform.position;
+	}
+
+	// 0.02秒のインターバルで更新.
+	void FixedUpdate()
+	{
+		Vector3 offset = new Vector3 (0, 0, Mathf.Sin(Time.time) * 0.5f);
+		GetComponent<Rigidbody>().MovePosition(origin + offset);
+	}
+
+}
