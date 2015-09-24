4e580b4fa7e88870e1ce615501f00aafbe8e15ae
shimazug <shimazug@marv.jp>
2015-09-24 17:24:27 +0900

    add ゲームのルール

--- Assets/Scripts/GameRule.cs
+++ Assets/Scripts/GameRule.cs
@@ -3,10 +3,69 @@ using System.Collections;
 
 public class GameRule : MonoBehaviour {
 
+	[SerializeField]GameObject SpawnerObject;
+	[SerializeField]GameObject GameRetryButton;
+	
+
 	void Awake()
 	{
 		// ゲーム画面サイズ固定.
 		Screen.SetResolution (1024, 768, false);
 	}
+
+	void Start()
+	{
+		GameRetryButton.SetActive (false);
+	}
+
+	void Update()
+	{
+		if (0 == SpawnerObject.GetComponent<Spawner> ().UsersCoinCounter) 
+		{
+			GameRetryButton.SetActive(true);
+		}
+	}
+
+	public void OnretryButton()
+	{
+		Application.LoadLevel ("Main");
+	}
+
+	/*
+	public void On5Num()
+	{
+		SpawnerObject.GetComponent<Spawner> ().UsersCoinCounter = 5;
+		for (int i = 0; i < NumButton.Length; i++) 
+		{
+			NumButton [i].SetActive (false);
+		}
+	}
+
+	public void On10Num()
+	{
+		SpawnerObject.GetComponent<Spawner> ().UsersCoinCounter = 10;
+		for (int i = 0; i < NumButton.Length; i++) 
+		{
+			NumButton [i].SetActive (false);
+		}
+	}
+
+	public void On20Num()
+	{
+		SpawnerObject.GetComponent<Spawner> ().UsersCoinCounter = 20;
+		for (int i = 0; i < NumButton.Length; i++) 
+		{
+			NumButton [i].SetActive (false);
+		}
+	}
+
+	public void On30Num()
+	{
+		SpawnerObject.GetComponent<Spawner> ().UsersCoinCounter = 30;
+		for (int i = 0; i < NumButton.Length; i++) 
+		{
+			NumButton [i].SetActive (false);
+		}
+	}*/
 
 }
