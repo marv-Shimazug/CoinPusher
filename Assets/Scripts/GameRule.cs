585f7bf25dee878e32c632734695b8fdde0889ea
shimazug <shimazug@marv.jp>
2015-09-24 14:27:57 +0900

    add ゲームの機能ファイル

---
+++ Assets/Scripts/GameRule.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameRule : MonoBehaviour {
+
+	void Awake()
+	{
+		// ゲーム画面サイズ固定.
+		Screen.SetResolution (1024, 768, false);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
