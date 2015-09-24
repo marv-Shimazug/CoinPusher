b4ce4e21c4559a40cf2bdc42d4b794251e918640
shimazug <shimazug@marv.jp>
2015-09-24 16:55:34 +0900

    add スコア表示

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Scorer : MonoBehaviour {
 
 	// 累計獲得枚数.
 	private int ScoreCount;
 
+	[SerializeField]GameObject SpawnerObject;
+
+	[SerializeField]Text AllCounter;
+
+	void Update()
+	{
+		TextIndicator ();
+	}
+
 	void OnTriggerEnter(Collider col)
 	{
-		ScoreCount += CountUp (col.GetComponent<Coin>().GetMyColor);
+		int addCount = CountUp (col.GetComponent<Coin>().GetMyColor);
+		ScoreCount += addCount;
+		SpawnerObject.GetComponent<Spawner> ().UsersCoinCounter += addCount;
+		SpawnerObject.GetComponent<Spawner>().AddBounusCoin(BounusCounter());
 		Destroy (col.gameObject);
 	}
 
@@ -24,6 +37,23 @@ public class Scorer : MonoBehaviour {
 			add = 1;
 		}
 		return add;
+	}
+
+	// ボーナス発生枚数.
+	int BounusCounter()
+	{
+		int retCoinNum = 0;
+		if(0 == ScoreCount % 10)
+		{
+			retCoinNum = Random.Range(30, 200);
+		}
+		return retCoinNum;
+	}
+
+	void TextIndicator()
+	{
+		AllCounter.text = "累計獲得枚数：" + ScoreCount.ToString() + "\n"
+			+"所持枚数：" + SpawnerObject.GetComponent<Spawner>().UsersCoinCounter + "\n";
 	}
 
 }
