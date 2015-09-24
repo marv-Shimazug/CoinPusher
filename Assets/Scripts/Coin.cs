897509a63ad74caf906bf1ff2efcd8e60b845b69
shimazug <shimazug@marv.jp>
2015-09-24 16:16:54 +0900

    add コイン情報

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	/// <summary>
+	/// 色.
+	/// true:金  false:銀
+	/// </summary>
+	private bool MyColor;
+	public bool GetMyColor{get{return MyColor;}}
+
+	/// <summary>
+	/// 色の設定.
+	/// </summary>
+	/// <param name="color">If set to <c>true</c> color.</param>
+	public void SetColor(bool color)
+	{
+		MyColor = color;
+	}
+}
